ff4fdf7e667a8ba1f19e56c059bca5e74e703dd4
KadTheAad <69099988+KadTheAad@users.noreply.github.com>
2024-03-21 09:52:09 +0400

    Update buildandrun.sh

--- buildandrun.sh
+++ buildandrun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /Users/khaledadib/harp-java 
+cd ~/harp-java 
 mvn package
-java -jar /Users/khaledadib/harp-java/target/HARP-1.0-SNAPSHOT-shaded.jar 
+java -jar ~/harp-java/target/HARP-1.0-SNAPSHOT-shaded.jar 
